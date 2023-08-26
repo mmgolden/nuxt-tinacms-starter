810724764af9227ebf387dc0691296090ba0f0a3
Melinda Golden <info@melindagolden.com>
2023-08-26 13:01:36 -0400

    chore: install ESLint, Prettier, and Tina CMS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content']
-})
+  modules: ["@nuxt/content"],
+
+  css: ["~/assets/main.css"],
+});
---
+++ tina/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || "master";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.NUXT_TINA_CLIENT_ID || "", // Get this from tina.io
+  token: process.env.NUXT_TINA_TOKEN || "", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
